6c7303b73520801829ee32fac40759b1e4278bf0
761468 <761468@PC452233.cts.com>
2019-08-22 17:42:24 +0530

    New Test Case - Sprint 5
    Test Deal Page Function.

--- src/test/java/com/mop/qa/pageobject/CreatedDealPage.java
+++ src/test/java/com/mop/qa/pageobject/CreatedDealPage.java
@@ -2,6 +2,7 @@ package com.mop.qa.pageobject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 
@@ -11,11 +12,40 @@ public class CreatedDealPage extends PageBase {
 	public CreatedDealPage(RemoteWebDriver remoteDriver) {
 		super(remoteDriver);
 	}
+
 	@FindBy(xpath = "//a[@title='Quick Actions']")
 	private WebElement quickAction;
 	@FindBy(xpath = "//button[text()=' New Document ']")
 	private WebElement newDocument;
-	
+
+	// ==========Added WebElements==========//
+	@FindBy(xpath = "//span[@class='nav-label']/following-sibling::mat-form-field")
+	private WebElement nxtMStoneDDown;
+	@FindBy(xpath = "//div[contains(@class,'mat-select-panel mat-primary')]")
+	private WebElement MstoneDdownList;
+
+	@FindBy(xpath = "//span[text()=' ONE PAGER']")
+	private WebElement optOnePager;
+	@FindBy(xpath = "//span[text()=' PRELIMINARY IC NOTE']")
+	private WebElement optPreICNote;
+	@FindBy(xpath = "//span[text()=' PRE-DD IC NOTE']")
+	private WebElement optPreDDICNote;
+	@FindBy(xpath = "//span[text()=' POST-DD IC NOTE']")
+	private WebElement optPostDDICNote;
+	@FindBy(xpath = "//span[text()=' CLOSED']")
+	private WebElement optClosed;
+	@FindBy(xpath = "//span[text()=' PASSED']")
+	private WebElement optPassed;
+
+	@FindBy(xpath = "//textarea[@placeholder='Reasons']")
+	private WebElement reasons;
+	@FindBy(xpath = "//span[text()='PASS DEAL']")
+	private WebElement passDealBtn;
+	@FindBy(xpath = "//h4[text()='Reason for Passing the Deal']/parent::div/following-sibling::div")
+	private WebElement passReasonTxt;
+
+	String Option = "//mat-option[OPTION]";
+
 	public void createNewDocument(RemoteWebDriver driver) throws Exception {
 		do {
 			Thread.sleep(1000);
@@ -24,5 +54,84 @@ public class CreatedDealPage extends PageBase {
 		waitForVisibilityOfElement(newDocument);
 		click(newDocument, "New Document");
 		Thread.sleep(1000);
+	}
+
+	public void dropDown(RemoteWebDriver driver) throws Exception {
+		Actions action = new Actions(driver);
+		Thread.sleep(1000);
+		if (driver.findElements(By.xpath("//span[@class='nav-label']/following-sibling::mat-form-field")).size() > 0) {
+			click(nxtMStoneDDown, "Next Milestone Dropdown");
+			Thread.sleep(500);
+		}
+		if (driver.findElements(By.xpath("//div[contains(@class,'mat-select-panel mat-primary')]")).size() > 0)
+			assertTrue("Next Milestone Drop-Down is Displayed.");
+		else
+			assertFalse("Next Milestone Drop-Down is not Displayed.");
+
+		if (optOnePager.isDisplayed()) {
+			action.moveToElement(optOnePager).perform();
+			assertTrue("One Pager Option is Displayed");
+		} else
+			assertFalse("One Pager Option is not Displayed");
+
+		if (optPreICNote.isDisplayed()) {
+			action.moveToElement(optPreICNote).perform();
+			assertTrue("Pre IC Note Option is Displayed");
+		} else
+			assertFalse("Pre IC Note Option not is Displayed");
+
+		if (optPreDDICNote.isDisplayed()) {
+			action.moveToElement(optPreDDICNote).perform();
+			assertTrue("Pre DD-IC Note Option is Displayed");
+		} else
+			assertFalse("Pre DD-IC Note Option not is Displayed");
+
+		if (optPostDDICNote.isDisplayed()) {
+			action.moveToElement(optPostDDICNote).perform();
+			assertTrue("Post DD-IC Note option is Displayed");
+		} else
+			assertFalse("Post DD-IC Note option is Displayed");
+
+		if (optClosed.isDisplayed()) {
+			action.moveToElement(optClosed).perform();
+			assertTrue("Closed Option is Displayed");
+		} else
+			assertFalse("Closed Option is not Displayed");
+
+		if (optPassed.isDisplayed()) {
+			action.moveToElement(optPassed).perform();
+			assertTrue("Passed Option is Displayed");
+		} else
+			assertFalse("Passed Option is not Displayed");
+	}
+
+	public void passDeal(RemoteWebDriver driver, String passReason) throws Exception {
+		Actions action = new Actions(driver);
+		action.moveToElement(optPassed).click().build().perform();
+		Thread.sleep(500);
+		if (driver.findElements(By.xpath("//app-passing-reason")).size() > 0) {
+			assertTrue("Passing Reason Pop-up Window is displayed");
+			click(reasons, "Reasons TextArea");
+			Thread.sleep(650);
+			enterText(reasons, passReason, "Pass Reason");
+			action.moveToElement(passDealBtn).click().build().perform();
+			Thread.sleep(1000);
+			if (driver.findElements(By.xpath("//h4[text()='Reason for Passing the Deal']")).size() > 0) {
+				if (passReasonTxt.getText().trim().equals(passReason))
+					assertTrue("Passing Reason is Saved and displayed with heading as 'Reason for Passing the Deal'");
+				else
+					assertFalse("Passing Reason is Not Displayed.");
+			}
+			click(nxtMStoneDDown, "Next Milestone Drop-Down List");
+			Thread.sleep(650);
+			int items = driver.findElements(By.xpath("//mat-option")).size();
+			int itemsDisabled = driver.findElements(By.xpath("//mat-option[contains(@class,'mat-option-disabled')]"))
+					.size();
+			if (items == itemsDisabled)
+				assertTrue("All "+items+" Options in Drop-Down List are Disabled and change is not possible.");
+			else
+				assertFalse("All "+items+" Options in Drop-Down List are not Disabled.");
+		} else
+			assertFalse("Passing Reason Pop-up Window is not displayed");
 	}
 }
---
+++ src/test/java/com/mop/qa/test/bvt/TestDealPage.java
@@ -0,0 +1,73 @@
+package com.mop.qa.test.bvt;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import com.mop.qa.pageobject.BasicDealDetails;
+import com.mop.qa.pageobject.CompanyFinancials;
+import com.mop.qa.pageobject.CompanyInformation;
+import com.mop.qa.pageobject.CreatedDealPage;
+import com.mop.qa.pageobject.DealHome;
+import com.mop.qa.pageobject.IndustryInformation;
+import com.mop.qa.pageobject.InvestmentHome;
+import com.mop.qa.testbase.TestBase;
+
+public class TestDealPage extends TestBase {
+	private static final Logger LOGGER = Logger.getLogger(TestDealPage.class.getName());
+
+	@Test
+	public void createDeal() throws Exception {
+		try {
+			InvestmentHome inst = new InvestmentHome(remoteDriver);
+			String startURL = rds.getValue("DATA", currentTest, "URL");
+			String uname = rds.getValue("DATA", currentTest, "UserName");
+			String pwd = rds.getValue("DATA", currentTest, "Password");
+			inst.launchApp(startURL, uname, pwd);
+			DealHome dHome = new DealHome(remoteDriver);
+			String projectName = rds.getValue("DATA", currentTest, "ProjectName");
+			String companyName = rds.getValue("DATA", currentTest, "CompanyName");
+			String sector = rds.getValue("DATA", currentTest, "Sector");
+			String flow = rds.getValue("DATA", currentTest, "Flow");
+			String filepath = rds.getValue("DATA", currentTest, "FilePath");
+			dHome.createDealHomePage(remoteDriver, projectName, companyName, sector, filepath);
+			Thread.sleep(1000);
+			dHome.selectFlow(remoteDriver, flow);
+			Thread.sleep(1000);
+			BasicDealDetails dealDetails = new BasicDealDetails(remoteDriver);
+			String subsector = rds.getValue("DATA", currentTest, "SubSector");
+			String source = rds.getValue("DATA", currentTest, "Source");
+			String dealsize = rds.getValue("DATA", currentTest, "DealSize");
+			String stake = rds.getValue("DATA", currentTest, "Stake");
+			String stakePercent = rds.getValue("DATA", currentTest, "StakePercent");
+			Thread.sleep(1000);
+			dealDetails.enterDealDetails(remoteDriver, subsector, source, dealsize, stake, stakePercent);
+			Thread.sleep(1000);
+			CompanyInformation companyInfo = new CompanyInformation(remoteDriver);
+			String cmpInfo = rds.getValue("DATA", currentTest, "CompanyInfoBrief");
+			String leader = rds.getValue("DATA", currentTest, "CurrentLeaderhip");
+			String shareHolder = rds.getValue("DATA", currentTest, "KeyShareHolders");
+			String percentage = rds.getValue("DATA", currentTest, "SharePercentage");
+			companyInfo.enterCompanyDetails(remoteDriver, cmpInfo, leader, shareHolder, percentage);
+			CompanyFinancials compFin = new CompanyFinancials(remoteDriver);
+			compFin.enterCompanyDetails();
+			IndustryInformation industryInfo = new IndustryInformation(remoteDriver);
+			String indSize = rds.getValue("DATA", currentTest, "IndustrySize");
+			String trgtMktShare = rds.getValue("DATA", currentTest, "TargetCompMarketShare");
+			String last3year = rds.getValue("DATA", currentTest, "Last3Year");
+			String last5year = rds.getValue("DATA", currentTest, "Last5Year");
+			String pred3year = rds.getValue("DATA", currentTest, "Predicted3Year");
+			String pred5year = rds.getValue("DATA", currentTest, "Predicted5Year");
+			String competitorName = rds.getValue("DATA", currentTest, "CompetitorName");
+			String percent = rds.getValue("DATA", currentTest, "IndSharePercentage");
+			String flows = rds.getValue("DATA", currentTest, "Flows");
+			industryInfo.enterIndustryDetails(remoteDriver, projectName, indSize, trgtMktShare, last3year, last5year,
+					pred3year, pred5year, competitorName, percent, flows);
+			// ==========DEAL CREATED==========//
+			CreatedDealPage dealPG = new CreatedDealPage(remoteDriver);
+			dealPG.dropDown(remoteDriver);
+			dealPG.passDeal(remoteDriver, "Sample Reason for Passing The Deal!");
+		} catch (Exception e) {
+			LOGGER.info(e);
+		}
+	}
+}
